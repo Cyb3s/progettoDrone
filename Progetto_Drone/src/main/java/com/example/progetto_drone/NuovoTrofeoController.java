491ebf0dcadce259354c2283347ee59e896936a4
Cyb3s <123Ivancasotti@gmail.com>
2025-04-16 13:07:53 +0200

    Creato il layout della finestra di creazione del nuovo trofeo

---
+++ Progetto_Drone/src/main/java/com/example/progetto_drone/NuovoTrofeoController.java
@@ -0,0 +1,32 @@
+package com.example.progetto_drone;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class NuovoTrofeoController {
+
+    @FXML
+    private Button button_annulla;
+
+    @FXML
+    private Button button_conferma;
+
+    @FXML
+    private ChoiceBox<?> choice_anno;
+
+    @FXML
+    private ChoiceBox<?> choice_tLanci;
+
+    @FXML
+    private TextField text_nGare;
+
+    @FXML
+    private TextField text_nLanci;
+
+    @FXML
+    private TextField text_nome;
+
+}
+
