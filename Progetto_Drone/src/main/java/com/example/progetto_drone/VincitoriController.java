fea79a5e06ba5e8c871be9713c70bc4c13b6728a
torre_i <s.iacopo.torre@polofermigiorgi.it>
2025-04-16 13:12:17 +0200

    Classi controller delle mie finestre assegnate e relativo fxml con lo scheletro iniziale: nodi, posizione e rinominazione

---
+++ Progetto_Drone/src/main/java/com/example/progetto_drone/VincitoriController.java
@@ -0,0 +1,39 @@
+package com.example.progetto_drone;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class VincitoriController {
+    @FXML
+    private Button btn_indietro;
+
+    @FXML
+    private TableColumn<?, ?> tbc_cognome;
+
+    @FXML
+    private TableColumn<?, ?> tbc_nome;
+
+    @FXML
+    private TableColumn<?, ?> tbc_puntiTotali;
+
+    @FXML
+    private TableView<?> tbw_tabella;
+
+    @FXML
+    private TextField txf_primoClassificato;
+
+    @FXML
+    private TextField txf_secondoClassificato;
+
+    @FXML
+    private TextField txf_terzoClassificato;
+
+    @FXML
+    void onIndietro(ActionEvent event) {
+
+    }
+}
