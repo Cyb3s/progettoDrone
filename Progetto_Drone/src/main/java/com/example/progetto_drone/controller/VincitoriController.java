e9e9e0f012ac7c8b97e979ebc4930d848a3d7c37
Iacopo Torre <s.iacopo.torre@polofermigiorgi.it>
2025-05-03 13:31:56 +0200

    settato la tabella nel metodo vincitori controller, per continuare ho bisogno della lista che dovro sortare e mettere nella tabella, successivamente i 3 vincitori nelle label a destra

--- Progetto_Drone/src/main/java/com/example/progetto_drone/controller/VincitoriController.java
+++ Progetto_Drone/src/main/java/com/example/progetto_drone/controller/VincitoriController.java
@@ -1,27 +1,32 @@
 package com.example.progetto_drone.controller;
 
+import com.example.progetto_drone.ChangeWindow;
+import com.example.progetto_drone.altro.Trofeo;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 public class VincitoriController {
     @FXML
     private Button btn_indietro;
 
     @FXML
-    private TableColumn<?, ?> tbc_cognome;
+    private TableColumn<Trofeo, String> tbc_cognome;
 
     @FXML
-    private TableColumn<?, ?> tbc_nome;
+    private TableColumn<Trofeo, String> tbc_nome;
 
     @FXML
-    private TableColumn<?, ?> tbc_puntiTotali;
+    private TableColumn<Trofeo, String> tbc_puntiTotali;
 
     @FXML
-    private TableView<?> tbw_tabella;
+    private TableView<Trofeo> tbw_tabella;
 
     @FXML
     private TextField txf_primoClassificato;
@@ -33,7 +38,23 @@ public class VincitoriController {
     private TextField txf_terzoClassificato;
 
     @FXML
-    void onIndietro(ActionEvent event) {
+    public void initialize(){
+        //settare le label primo, secondo e terzo con il nome e cognome del vincitore
+        //scrivere la classifica nella tabella
 
+        // Popolare la TableView con la classifica completa
+        tbw_tabella.setItems(FXCollections.observableArrayList(/*classifica*/));
+
+        // Configurare le colonne della TableView
+        tbc_nome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        tbc_cognome.setCellValueFactory(new PropertyValueFactory<>("cognome"));
+        tbc_puntiTotali.setCellValueFactory(new PropertyValueFactory<>("puntiTotali"));
+    }
+
+    @FXML
+    void onIndietro(ActionEvent event) {
+        ChangeWindow.changeWindow(" menu.fxml","Menu' Trofei");
+        Stage ss=(Stage) btn_indietro.getScene().getWindow();
+        ss.close();
     }
 }
